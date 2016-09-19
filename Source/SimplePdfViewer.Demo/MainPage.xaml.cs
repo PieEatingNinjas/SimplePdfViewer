23dbc79cf03b943d7b4cf7a77b015fc3e86967b1
Pieter Nijs <pieternijs@live.be>
2016-09-19 13:09:53 +0200

    Initial commit

---
+++ Source/SimplePdfViewer.Demo/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SimplePdfViewer.Demo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Uri Source { get; set; }
+
+        public async void OpenLocal()
+        {
+            var uri = new Uri("ms-appx:///Assets/pdffile.pdf");
+
+            Source = uri;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Source)));
+        }
+
+        public async void OpenRemote()
+        {
+            var uri = new Uri("http://www.adobe.com/content/dam/Adobe/en/accessibility/products/acrobat/pdfs/acrobat-x-accessible-pdf-from-word.pdf");
+
+            Source = uri;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Source)));
+        }
+    }
+}
---
+++ Source/SimplePdfViewer/SimplePdfViewerControl.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.Data.Pdf;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace SimplePdfViewer
+{
+    public sealed partial class SimplePdfViewerControl : UserControl
+    {
+        public SimplePdfViewerControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public Uri Source
+        {
+            get { return (Uri)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourceProperty =
+            DependencyProperty.Register("Source", typeof(Uri), typeof(SimplePdfViewerControl), new PropertyMetadata(null, OnSourceChanged));
+
+        public bool AutoLoad { get; set; }
+
+        private static void OnSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            ((SimplePdfViewerControl)d).OnSourceChanged();
+        }
+
+        private void OnSourceChanged()
+        {
+            if(AutoLoad)
+            {
+                LoadAsync();
+            }
+        }
+
+        public async Task LoadAsync()
+        {
+            if(Source == null)
+            {
+                PdfPages.Clear();
+            }
+            else
+            {
+                if(Source.IsFile || !CheckIsHttp(Source))
+                {
+                    await LoadFromLocalAsync();
+                }
+                else if(CheckIsHttp(Source))
+                {
+                    await LoadFromRemoteAsync();
+                }
+                else
+                {
+                    //problem
+                }
+            }
+        }
+
+        private bool CheckIsHttp(Uri uri)
+        {
+            if (uri != null)
+            {
+                var str = uri.ToString();
+                return str.StartsWith("http://") || str.StartsWith("https://");
+            }
+            return false;
+        }
+
+        private async Task LoadFromRemoteAsync()
+        {
+            HttpClient client = new HttpClient();
+            var stream = await
+                client.GetStreamAsync(Source);
+            var memStream = new MemoryStream();
+            await stream.CopyToAsync(memStream);
+            memStream.Position = 0;
+            PdfDocument doc = await PdfDocument.LoadFromStreamAsync(memStream.AsRandomAccessStream());
+
+            Load(doc);
+        }
+
+        private async Task LoadFromLocalAsync()
+        {
+            StorageFile f = await
+                StorageFile.GetFileFromApplicationUriAsync(Source);
+            PdfDocument doc = await PdfDocument.LoadFromFileAsync(f);
+
+            Load(doc);
+        }
+
+        async void Load(PdfDocument pdfDoc)
+        {
+            PdfPages.Clear();
+
+            for (uint i = 0; i < pdfDoc.PageCount; i++)
+            {
+                BitmapImage image = new BitmapImage();
+
+                var page = pdfDoc.GetPage(i);
+
+                using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+                {
+                    await page.RenderToStreamAsync(stream);
+                    await image.SetSourceAsync(stream);
+                }
+
+                PdfPages.Add(image);
+            }
+        }
+
+        internal ObservableCollection<BitmapImage> PdfPages
+        {
+            get;
+            set;
+        } = new ObservableCollection<BitmapImage>();
+    }
+}
